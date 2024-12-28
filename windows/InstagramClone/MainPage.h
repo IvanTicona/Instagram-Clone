290556545f2ef158d882d1c5b3797f2c217856a4
Dorian Ivan Ticona Vega <66503891+IvanTicona@users.noreply.github.com>
2024-12-27 22:05:25 -0400

    Created

---
+++ windows/InstagramClone/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::InstagramClone::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::InstagramClone::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
